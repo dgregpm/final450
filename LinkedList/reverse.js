817192b7b4a06fe4a767b915d78f6dd13e8c661f
dgregpm <dgregpm@gmail.com>
2023-07-12 17:58:23 -0400

    Partial Solution

---
+++ LinkedList/reverse.js
@@ -0,0 +1,35 @@
+/**
+ * @param {Node} head
+ * @returns {Node}
+*/
+
+
+class Node{
+    constructor(data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class Solution {
+    //Function to reverse a linked list. Iterative Solution.
+    reverseList(head)
+    {
+        let h = head;
+        let n = 1;
+
+        while(h.next !== null){
+            h = h.next;
+            n++;
+        }
+        result = h;
+        while(n>0){
+            h = head;
+            for(let i = 0;i<n;i++){
+                
+            }
+        }
+    }
+    
+}
